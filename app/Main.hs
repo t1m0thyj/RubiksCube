5a750e47f5cd221ea748563ab2446d96d528f079
Timothy Johnson <tandrewjohnson@outlook.com>
2018-12-27 22:10:52 -0500

    Simple reduction function for one side of cube

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,5 @@ module Main where
 
 import Lib
 
-data Nat = Z | S Nat
-
-add :: Nat -> Nat -> Nat
-add Z y = y
-add (S x) y = S (add x y)
-
 main :: IO ()
-main = putStrLn "Aardvark"
+main = putStrLn $ show $ reduce [CW, CCW, CCW]
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,60 +2,25 @@
 
 module Lib where
 
-data NTurn = Z | O | T | Th
+data NTurns = Z | N1 | N2 | N3 deriving Show
 
-data Turn = NoTurn | CW | CCW | And Turn Turn deriving (Eq, Show)
+data Turn = CW | CCW
 
-assoc :: Turn -> Turn
-assoc (And x y) =
-  let x' = assoc x
-  in case x' of
-    (And a b) -> And a (And b y)
-    _ -> And x' y
-assoc x = x
+turnCW :: NTurns -> NTurns
+turnCW Z = N1
+turnCW N1 = N2
+turnCW N2 = N3
+turnCW N3 = Z
 
-iso :: Turn -> Turn -> Bool
-iso x y = assoc x == assoc y
+turnCCW :: NTurns -> NTurns
+turnCCW Z = N3
+turnCCW N1 = Z
+turnCCW N2 = N1
+turnCCW N3 = N2
 
-instance Semigroup Turn where
-    x <> y = And x y
+turn :: Turn -> NTurns -> NTurns
+turn CW = turnCW
+turn CCW = turnCCW
 
-instance Monoid Turn where
-   mempty = NoTurn
-
-naiveInverse :: Turn -> Turn -> Bool
-naiveInverse CW CCW = True
-naiveInverse CCW CW = True
-naiveInverse _ _ = False
-
-reduceByInv :: Turn -> Turn
-reduceByInv (And x a@(And y z)) =
-  if naiveInverse x y
-  then reduceByInv z
-  else And x (reduceByInv a)
-reduceByInv (And x y) =
-  if naiveInverse x y
-  then NoTurn
-  else And x (reduceByInv y)
-reduceByInv x = x
-
-reduceById :: Turn -> Turn
-reduceById (And NoTurn x) = reduceById x
-reduceById (And x NoTurn) = reduceById x
-reduceById (And x y) = And (reduceById x) (reduceById y)
-reduceById x = x
-
-reduceByRing :: Turn -> Turn
-reduceByRing (And a b'@(And b (And c (And d e)))) =
-  if a == b && b == c && c == d
-  then reduceByRing e
-  else And a (reduceByRing b')
-reduceByRing (And a (And b (And c d))) =
-  if a == b && b == c && c == d
-  then NoTurn
-  else And a (reduceByRing b')
-
-
--- cw <> cw <> cw <> cw = NoTurn
--- NoTurn <> NoTurn <> NoTurn <> NoTurn = NoTurn
--- ccw <> ccw <> ccw <> ccw = NoTurn
+reduce :: [Turn] -> NTurns
+reduce = foldr turn Z
